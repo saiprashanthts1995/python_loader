882c4595d522517335fb4718d073569e7f8947db
Sai Prashanth T S <saiprashanthts@gmail.com>
2020-07-05 09:42:05 +0530

    added an functionality to add timer as decorator

--- utils.py
+++ utils.py
@@ -1,1 +1,28 @@
 from mysql import connector as mc
+import datetime
+
+def mysql_connection(env):
+    pass
+
+
+def postgres_connection(env):
+    pass
+
+
+def read_config(env):
+    pass
+
+
+def tables_to_be_loaded():
+    pass
+
+
+def time(method):
+    def time_method(*args, **kwargs):
+        ts = datetime.datetime.now()
+        result = method(*args, **kwargs)
+        te = datetime.datetime.now()
+        print('Total time took is {}'.format(te-ts))
+        return result
+    return time_method
+
