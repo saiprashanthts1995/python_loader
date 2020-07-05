704cf086e08a0e53673453d51b69c50536a08797
Sai Prashanth T S <saiprashanthts@gmail.com>
2020-07-05 10:03:26 +0530

    added the logic for read_config

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 from mysql import connector as mc
 import datetime
+import json
+import pandas as pd
+
 
 def mysql_connection(env):
     pass
@@ -9,8 +12,10 @@ def postgres_connection(env):
     pass
 
 
-def read_config(env):
-    pass
+def read_config(env, database_type):
+    with open('config.json') as config_file:
+        config_file_content = json.load(config_file)
+    return config_file_content[env.upper()][database_type.upper()]
 
 
 def tables_to_be_loaded():
@@ -31,3 +36,7 @@ def modern_printer(message):
     print('=' * 100)
     print('{}'.format(message))
     print('=' * 100)
+
+
+if __name__ == '__main__':
+    print(read_config('QA', 'POSTGRES'))
